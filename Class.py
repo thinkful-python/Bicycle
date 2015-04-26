ec894fea538f85afd7ba8d97ab589d471fdd7913
dom <dom@thedom.io>
2015-04-26 05:34:54 +0000

    inital commit

--- Class.py
+++ Class.py
@@ -14,7 +14,11 @@ class Shop(object):
 		self.name = name
 		self.margin = margin
 		self.inventory = []
-		
+	def show_inventory(self):
+		for bike in self.inventory: 
+			print ("%s at cost %d" %(bike.name, bike.cost * self.margin ))
+			# print "{} at cost {}".format(bike.name, bike.cost)
+
 	def add_inventory(self, bicycle):
 		self.inventory.append(bicycle)
 
@@ -30,8 +34,9 @@ hybrid_bike = Bicycle("Hybrid Bike", 25, 650)
 road_bike = Bicycle("Road Bike", 25, 120)
 electric_bike = Bicycle("Electric Bike", 25, 750)
 
-shop = Shop("Cycle House", 20)
+shop = Shop("Cycle House", 1.20)
 shop.add_inventory(mountain_bike)
 shop.add_inventory(hybrid_bike)
 shop.add_inventory(road_bike)
 shop.add_inventory(electric_bike)
+shop.show_inventory()
