a05ba44852062d6a2e36717873a59fe1f46c56e5
dom <dom@thedom.io>
2015-04-30 06:45:13 +0000

    whats the indentation error?

--- Class.py
+++ Class.py
@@ -9,7 +9,6 @@ class Bicycle(object):
 	def is_affordable(self,customer):
 		return self.cost <= customer.wallet
 
-
 class Shop(object):
 	""" Information about the bike shop """
 	def __init__(self, name, margin):
@@ -20,8 +19,10 @@ class Shop(object):
 		for bike in self.inventory: 
 			print ("%s at cost %d" %(bike.name, bike.cost * self.margin ))
 			# print "{} at cost {}".format(bike.name, bike.cost)
-
-	def add_inventory(self, bicycle):
+  def price(self,bicycle):
+      return bicycle.cost * self.margin
+    
+  def add_inventory(self, bicycle):
 		self.inventory.append(bicycle)
 
 class Customer(object):
@@ -31,16 +32,14 @@ class Customer(object):
 		self.wallet = wallet
 
 bikes = []    
-bikes.append(("Mountain Bike", 35, 120))
-bikes.append(("Hybrid Bike", 25, 150))
-bikes.append(("Road Bike", 25, 100))
-bikes.append(("Electric Bike", 25, 350))
+bikes.append(Bicycle("Mountain Bike", 35, 120))
+bikes.append(Bicycle("Hybrid Bike", 25, 150))
+bikes.append(Bicycle("Road Bike", 25, 100))
+bikes.append(Bicycle("Electric Bike", 25, 350))
+
 
 shop = Shop("Cycle House", 1.20)
-shop.add_inventory(mountain_bike)
-shop.add_inventory(hybrid_bike)
-shop.add_inventory(road_bike)
-shop.add_inventory(electric_bike)
+shop.inventory = bikes
 shop.show_inventory()
 
 customers = []
@@ -51,6 +50,7 @@ customers.append(Customer("Rayden", 1000))
 for customer in customers:
 	print "{0} can afford these".format(customer.name)
 	for bike in shop.inventory:
-		if bike.is_affordable(customer): 
-			print bike.name
+		if shop.price(bike) <= customer.wallet
+      print bike
+      
 
