23add6589b47f09a42af5d130a30fd3ae4fc7e87
dom <dom@thedom.io>
2015-05-12 06:50:30 +0000

    need help removing from inventory

--- Bicycles.py
+++ Bicycles.py
@@ -1,30 +1,33 @@
 class Bicycle(object):
-	""" Information and specifications about a bike """
-	def __init__(self, name, weight, cost):
-		self.name = name
-		self.weight = weight
-		self.cost = cost
-	def is_affordable(self,customer):
-		return self.cost <= customer.wallet
+        """ Information and specifications about a bike """
+        def __init__(self, name, weight, cost):
+                self.name = name
+                self.weight = weight
+                self.cost = cost
+        def is_affordable(self,customer):
+                return self.cost <= customer.wallet
 
 class Shop(object):
-	""" Information about the bike shop """
-	def __init__(self, name, margin):
-		self.name = name
-		self.margin = margin
-		self.inventory = []
-	def show_inventory(self):
-		for bike in self.inventory: 
-			print ("%s at cost %d" %(bike.name, bike.cost * self.margin ))
-			# print "{} at cost {}".format(bike.name, bike.cost)
-	
-	def price(self,bicycle):
-		return bicycle.cost * self.margin
-	def add_inventory(self, bicycle):
-		self.inventory.append(bicycle)
+        """ Information about the bike shop """
+        def __init__(self, name, margin):
+                self.name = name
+                self.margin = margin
+                self.inventory = dict()
+        def show_inventory(self):
+                for bike, count in self.inventory.iteritems():
+                        print ("{0} {1}s left at {2}".format(count, bike.name, bike.cost * self.margin))
+
+        def price(self,bicycle):
+                return bicycle.cost * self.margin
+        def add_inventory(self, bicycle, count):
+                self.inventory[bicycle] = count
 
 class Customer(object):
-	""" Information about the customers """
-	def __init__(self, name, wallet):
-		self.name = name
-		self.wallet = wallet
+        """ Information about the customers """
+        def __init__(self, name, wallet):
+                self.name = name
+                self.wallet = wallet
+                self.inventory = dict()
+        def purchase(self, bike, bicycle):
+                self.bike = (bike)
+                self.purchase = self.inventory[bicycle]-1
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import Bicycles
+from bicycles import Bicycle, Shop, Customer
 
-bikes = []    
+bikes = []
 bikes.append(Bicycle("Mountain Bike", 35, 120))
 bikes.append(Bicycle("Hybrid Bike", 25, 150))
 bikes.append(Bicycle("Road Bike", 25, 100))
@@ -8,7 +8,8 @@ bikes.append(Bicycle("Electric Bike", 25, 350))
 
 
 shop = Shop("Cycle House", 1.20)
-shop.inventory = bikes
+for bike in bikes:
+  shop.add_inventory(bike, 5)
 shop.show_inventory()
 
 customers = []
@@ -17,12 +18,11 @@ customers.append(Customer("Angela", 500))
 customers.append(Customer("Rayden", 1000))
 
 for customer in customers:
-	print "{0} can afford these".format(customer.name)
-	for bike in shop.inventory:
-		if shop.price(bike) <= customer.wallet:
-			print bike.name
-			can_afford = bike
-	print("{0} can afford {1} at {2}".format(customer.name, can_afford.name, shop.price(can_afford)))
-  
-	customer.wallet -= (shop.price(can_afford))
-	print ("After bike purchase {0} has {1} leftover".format(customer.name, customer.wallet)) 
+        print "{0} can afford these".format(customer.name)
+        for bike in shop.inventory:
+                if shop.price(bike) <= customer.wallet:
+                        print bike.name
+                        can_afford = bike
+        customer.purchase(can_afford, shop)
+        print("{0} bought a {1} at {2} with {3} dollars left".format(customer.name, can_afford.name, shop.price(can_afford), customer.wallet))
+        shop.show_inventory()
